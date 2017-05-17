f52088d8f1e90140c4266f366c93aa60b0fc2b87
javipus <javier.prietop@estudiante.uam.es>
2017-05-17 19:21:15 +0200

    added modified functions

--- R/executeTuning.R
+++ R/executeTuning.R
@@ -35,9 +35,9 @@ executeTuning <- function(inPath, logFile=NA) {
         db <- readRDS(paste(inPath, dbname, sep=""))
 
         # Set all DTW parameters for tuning
-        pWindowTypes <- c("itakura", "sakoechiba", "slantedband")
-        pStepPatterns <- c("symmetric1", "symmetric2", "asymmetric")
-        pWindowSizes <- c(1, 5, 10, 30, 50, 100)
+        pWindowTypes <- c("sakoechiba", "slantedband")
+        pStepPatterns <- c("symmetric1", 'symmetric2')
+        pWindowSizes <- c(1, 5, 10)
         dtwParameters <- as.data.frame(expand.grid(windowType=pWindowTypes,
                                                    stepPattern=pStepPatterns,
                                                    windowSize=pWindowSizes))
